7e5967befb2665195863da8b9b4332e7037628b4
Gerhard Pegel <gpegel@gmail.com>
2020-01-06 21:06:06 +0100

    some minor adjustments

--- start.py
+++ start.py
@@ -1,14 +1,40 @@
 import os
 import docker
+import tarfile
+import time
 
 client = docker.from_env()
 
 print('Hello, ' + os.getlogin() + '! Please wait while all containers are starting.\n')
 
-zabbix_env = client.containers.run("zabbix/zabbix-appliance:latest",
+zabbix = client.containers.run("zabbix/zabbix-appliance:latest",
     name="zabbix-monitoring",
-    ports={'10051/tcp': 10051, '80/tcp': 81},
+    ports ={'80/tcp': 81},
     detach=True)
+
+def copy_to(src, dst):
+    name, dst = dst.split(':')
+    container = client.containers.get(name)
+
+    os.chdir(os.path.dirname(src))
+    srcname = os.path.basename(src)
+    tar = tarfile.open(src + '.tar', mode='w')
+    try:
+        tar.add(srcname)
+    finally:
+        tar.close()
+
+    data = open(src + '.tar', 'rb').read()
+    container.put_archive(os.path.dirname(dst), data)
+
+zabbix_agent = client.containers.run("zabbix/zabbix-agent:latest",
+    name="zabbix-agent",
+    links={'zabbix-monitoring': 'zabbix-server'},
+    detach=True)
+
+copy_to('/Users/gpegel/Git/prombix/zabbix_config/zabbix_agentd.conf', 'zabbix-agent:/etc/zabbix/zabbix_agentd.conf')
+
+time.sleep(15)
 
 grafana = client.containers.run("grafana/grafana:latest",
     name="grafana",
@@ -17,16 +43,17 @@ grafana = client.containers.run("grafana/grafana:latest",
     links={'zabbix-monitoring': 'zabbix-server'},
     detach=True)
 
-
 def get_ip(container, network_name="bridge"):
     nw = client.networks.get(network_name)
     ip = [_.attrs['NetworkSettings']['Networks']['bridge']['IPAddress'] for _ in nw.containers if _.id == container.id][0]
     return ip
 
-zabbix_env_ip = get_ip(zabbix_env)
+zabbix_ip = get_ip(zabbix)
+zabbix_agent_ip = get_ip(zabbix_agent)
 grafana_ip = get_ip(grafana)
 
-print("IP of Zabbix Server in Docker network is : " + zabbix_env_ip + "\n")
+print("IP of Zabbix Server in Docker network is : " + zabbix_ip + "\n")
+print("IP of Zabbix Agent in Docker network is : " + zabbix_agent_ip + "\n")
 print("IP of Grafana in Docker network is : " + grafana_ip + "\n")
 
 print("Containers started, have a nice day!\n")
