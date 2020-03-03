ced7ddcee404830cb9cfd2dea2b9dbe6a0342fef
Gerhard Pegel <gpegel@gmail.com>
2020-03-03 15:44:25 +0100

    node exporter up and running

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Feel free to fork, clone and/or contribute to this repo.
 ## What you need:
 
 1. docker installed
-2. python3 installed and working
+2. python3 installed and working. Current version I'm using is **3.6.1** 
 3. if you miss some modules, please run `pip3 install -r requirements.txt` to install all necesarry modules.
 
 ## How to set this up:
@@ -20,10 +20,16 @@ So to start MySQL, Zabbix Server, Zabbix Server Front-End, Zabbix Agent, ~~Prome
 
 To stop all containers you could use the `python3 stop_all.py` script but be aware, running this script wil stop and remove ALL of your running containers. Even the ones that weren't started by this project.
 
+## Access Zabbix from Grafana
+
+1. check if Zabbix plugin is enabled
+2. add http://localhost/api_jsonrpc.php as HTTP URL in Grafana datasource
+3. Have fun creating dashboards
+
 ### To-do list (subject to change)
 
 - [x] For some reason the Zabbix server does not start so 'Go Fix'.
-- [ ] Add Prometheus server + end-point running node-exporter https://github.com/prometheus/node_exporter
+- [x] Add Prometheus server + end-point running node-exporter https://github.com/prometheus/node_exporter
 - [x] Add Grafana server.
 - [x] Add Grafana server incl. Zabbix datasources.
 - [ ] Add Grafana server incl. dashboards.
--- start.py
+++ start.py
@@ -8,6 +8,7 @@ print('Hello, ' + os.getlogin() + '! Please wait while all containers are starti
 zabbix = client.containers.run("zabbix/zabbix-appliance:latest",
     name="zabbix-monitoring",
     ports={'80/tcp': 80},
+    network="bridge",
     detach=True)
 
 zabbix_agent = client.containers.run("zabbix/zabbix-agent:latest",
@@ -15,6 +16,7 @@ zabbix_agent = client.containers.run("zabbix/zabbix-agent:latest",
     links={'zabbix-monitoring': 'zabbix-server'},
     volumes={'/Users/gerhardpegel/Git/prombix/config/zabbix_config/': {'bind': '/etc/zabbix/', 'mode': 'ro'},
              '/Users/gerhardpegel/Git/prombix/config/zabbix_agentd.d/': {'bind': '/etc/zabbix/zabbix_agentd.d/', 'mode': 'ro'}},
+    network="bridge",
     detach=True)
 
 grafana = client.containers.run("grafana/grafana:latest",
@@ -22,12 +24,20 @@ grafana = client.containers.run("grafana/grafana:latest",
     ports={'3000/tcp': 3000},
     environment=["GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource,alexanderzobnin-zabbix-app"],
     links={'zabbix-monitoring': 'zabbix-server'},
+    network="bridge",
     detach=True)
 
 prometheus = client.containers.run("prom/prometheus:latest",
     name="prometheus",
     ports={'9090/tcp': 9090},
-    volumes={'/Users/gerhardpegel/Git/prombix/config/prometheus_config/': {'bind': '/etc/prometheus_config/', 'mode': 'ro'}},
+    volumes={'/Users/gerhardpegel/Git/prombix/config/prometheus_config/': {'bind': '/etc/prometheus/', 'mode': 'ro'}},
+    network="bridge",
+    detach=True)
+
+node_exporter = client.containers.run("quay.io/prometheus/node-exporter",
+    name="node-exporter",
+    # ports={'9100/tcp' : 9100},
+    network="bridge",
     detach=True)
 
 def get_ip(container, network_name="bridge"):
@@ -39,10 +49,12 @@ zabbix_ip = get_ip(zabbix)
 zabbix_agent_ip = get_ip(zabbix_agent)
 grafana_ip = get_ip(grafana)
 prometheus_ip = get_ip(prometheus)
+node_exporter_ip = get_ip(node_exporter)
 
 print("IP of Zabbix Server in Docker network is : " + zabbix_ip + "\n")
 print("IP of Zabbix Agent in Docker network is : " + zabbix_agent_ip + "\n")
 print("IP of Grafana in Docker network is : " + grafana_ip + "\n")
 print("IP of Prometheus in Docker network is : " + prometheus_ip + "\n")
+print("IP of Prometheus Node-Exporter in Docker network is : " + node_exporter_ip + "\n")
 
 print("Containers started, have a nice day!\n")
