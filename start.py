4fea776095aba730ac02ec57cb3a647ab42e40bc
Gerhard Pegel <gpegel@gmail.com>
2020-03-05 08:12:57 +0100

    add host automatically

--- start.py
+++ start.py
@@ -9,6 +9,7 @@ from pyzabbix import ZabbixAPI, ZabbixAPIException
 client = docker.from_env()
 
 path = '/Users/gerhardpegel/Git/prombix/config/zabbix_templates'
+zabbix_hosts_path = '/Users/gerhardpegel/Git/prombix/config/zabbix_hosts'
 
 print('Hello, ' + os.getlogin() + '! Please wait while all containers are starting.\n')
 
@@ -66,15 +67,66 @@ print("IP of Prometheus Node-Exporter in Docker network is : " + node_exporter_i
 print("Waiting 45 seconds for all containers to settle.\n")
 time.sleep(45)
 
-# I stole this part from https://github.com/lukecyca/pyzabbix
-# The hostname at which the Zabbix web interface is available
-ZABBIX_SERVER = 'http://0.0.0.0:80'
+zabbix_server = 'http://0.0.0.0:80'
+csvfile = open(zabbix_hosts_path+'/list.csv', 'r')
 
-zapi = ZabbixAPI(ZABBIX_SERVER)
+zapi = ZabbixAPI(zabbix_server)
 
 # Login to the Zabbix API
 #zapi.session.verify = False
 zapi.login("Admin", "zabbix")
+
+print("Connected to Zabbix API Version %s" % zapi.api_version())
+
+def get_hostgroup_id(hostgroup):
+    data = zapi.hostgroup.get(filter={'name': hostgroup})
+    if data != []:
+        hostgroupid = data[0]['groupid']
+    else:
+        raise Exception('Could not find hostgroupID for: ' + hostgroup)
+    return str(hostgroupid)
+
+def add_host(ip, hostname, hostgroups):
+    interface_list = [{
+        "type": 1,
+        "main": 1,
+        "useip": 1,
+        "ip": str(ip),
+        "dns": '',
+        "port": "10050"
+    }]
+
+    groups = list()
+
+    for g in hostgroups:
+        groups.append({"groupid": str(get_hostgroup_id(g))})
+
+
+    query = {
+        'host': str(hostname),
+        'groups': groups,
+        'proxy_hostid': '0',
+        'status': '0',
+        'interfaces': interface_list,
+        'inventory_mode': 1,
+        'inventory': {
+            'name': str(hostname)
+        }
+    }
+
+    result = zapi.host.create(**query)
+
+for l in csvfile.readlines():
+    if l.strip().startswith('#') or not ',' in l:
+        continue
+
+    myargs = l.strip().split(',')
+    try:
+        add_host(myargs[0], myargs[1], myargs[2].split(':'))
+        print("Added %s" %myargs[0])
+    except:
+        print("Failed to add %s" %myargs[0])
+        pass
 
 rules = {
     'applications': {
@@ -154,5 +206,8 @@ elif os.path.isfile(path):
                 print(e)
 else:
     print('I need a xml file')
+
+
+
 
 print("Containers started, have a nice day!\n")
